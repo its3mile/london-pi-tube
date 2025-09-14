de46f7bfbc8af06427dbb8b71b3db56de4308045
its3mile <81533043+its3mile@users.noreply.github.com>
2025-09-14 16:42:08 +0100

    fix: increase line_statuses vector size
    this fixes a minor issue of line_statuses not being deserialised as it was larger than expected (yay for the district line and the many closures this weekend!)

--- src/api_requests/models/status.rs
+++ src/api_requests/models/status.rs
@@ -6,13 +6,13 @@ use serde::Deserialize;
 
 use crate::api_requests::models::TFL_API_FIELD_LONG_STR_SIZE;
 
-pub const ARRAY_MAX_SIZE_STATUS_MODEL: usize = 1;
+pub const ARRAY_MAX_SIZE_STATUS_MODEL: usize = 4;
 
 #[derive(Deserialize, Debug, Format)]
 #[serde(rename_all = "camelCase")]
 pub struct Status {
     #[serde(rename = "$type")]
     pub _type: String<TFL_API_FIELD_LONG_STR_SIZE>,
-    pub line_statuses: Vec<LineStatus, 1>,
+    pub line_statuses: Vec<LineStatus, ARRAY_MAX_SIZE_STATUS_MODEL>,
     // Incomplete implementation, as much of the data is not required
 }
