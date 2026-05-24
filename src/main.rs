8346cc96ca467eb830dc61f0ab73293e9114db91
its3mile <81533043+its3mile@users.noreply.github.com>
2026-05-24 10:27:39 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 #![no_std]
 #![no_main]
+#![allow(async_fn_in_trait)]
 
+#[allow(unused)] // FIXME: remove when done
+use ::function_name::named;
+use assign_resources::assign_resources;
+use cyw43::JoinOptions;
 use cyw43::aligned_bytes;
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::peripherals::{DMA_CH0, DMA_CH1, PIO0};
+use embassy_net::{Config, StackResources};
+use embassy_rp::bind_interrupts;
+use embassy_rp::clocks::RoscRng;
+use embassy_rp::gpio::{Input, Level, Output, Pull};
+use embassy_rp::peripherals::{DMA_CH0, PIO0, SPI1};
 use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_rp::{bind_interrupts, gpio};
+use embassy_rp::spi;
+use embassy_rp::spi::Spi;
+use embassy_rp::{Peri, peripherals};
+use embassy_time::Delay;
 use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use epd_waveshare::epd3in7::{Display3in7, EPD3in7};
+use epd_waveshare::prelude::{DisplayRotation, WaveshareDisplay};
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -30,73 +44,195 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
+// Clock divider for the RM2 Module
+// This differs for between the Raspberry Pi Pico 1w/2w and Pimoroni Pico Plus 2w
+use fixed::FixedU32;
+use fixed::types::extra::U8;
+const CHIP_SPECIFIC_CLOCK_DIVIDER: FixedU32<U8> = RM2_CLOCK_DIVIDER;
+
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
-    DMA_IRQ_0 => embassy_rp::dma::InterruptHandler<DMA_CH0>, embassy_rp::dma::InterruptHandler<DMA_CH1>;
+    DMA_IRQ_0 => embassy_rp::dma::InterruptHandler<DMA_CH0>;
 });
 
 #[embassy_executor::task]
 async fn cyw43_task(
-    runner: cyw43::Runner<
-        'static,
-        cyw43::SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>,
-        // cyw43::Cyw43439,
-    >,
+    runner: cyw43::Runner<'static, cyw43::SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>>,
 ) -> ! {
     runner.run().await
 }
 
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'static>>) -> ! {
+    runner.run().await
+}
+
+assign_resources! {
+    display_resources: DisplayResources {
+        spi1: SPI1,
+        pin_9: PIN_9,
+        pin_8: PIN_8,
+        pin_13: PIN_13,
+        pin_12: PIN_12,
+        pin_11: PIN_11,
+        pin_10: PIN_10,
+    }
+    network_resources: NetworkResources {
+        pio0: PIO0,
+        dma_ch0: DMA_CH0,
+        pin_23: PIN_23,
+        pin_24: PIN_24,
+        pin_25: PIN_25,
+        pin_29: PIN_29,
+    }
+}
+
+#[named]
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let fw = aligned_bytes!("../cyw43-firmware/43439A0.bin");
-    let clm = aligned_bytes!("..//cyw43-firmware/43439A0_clm.bin");
-    let nvram = aligned_bytes!("../cyw43-firmware/nvram_rp2040.bin");
+    let fw: &cyw43::Aligned<cyw43::A4, [u8]> = aligned_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm: &cyw43::Aligned<cyw43::A4, [u8]> =
+        aligned_bytes!("..//cyw43-firmware/43439A0_clm.bin");
+    let nvram: &cyw43::Aligned<cyw43::A4, [u8]> =
+        aligned_bytes!("../cyw43-firmware/nvram_rp2040.bin");
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download ../../cyw43-firmware/43439A0.bin --binary-format bin --chip RP235x --base-address 0x10100000
-    //     probe-rs download ../../cyw43-firmware/43439A0_clm.bin --binary-format bin --chip RP235x --base-address 0x10140000
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    info!("{}: Starting main task...", function_name!());
+    let peripherals: embassy_rp::Peripherals = embassy_rp::init(Default::default());
+    let split_peripherals = split_resources!(peripherals);
 
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
+    // Spawn the task to update the display with predictions
+    info!("{}: Initialising display...", function_name!());
+
+    // Setup display pins and SPI bus
+    let pin_reset: Output<'_> = Output::new(split_peripherals.display_resources.pin_12, Level::Low);
+    let pin_cs = Output::new(split_peripherals.display_resources.pin_9, Level::High);
+    let pin_data_cmd: Output<'_> =
+        Output::new(split_peripherals.display_resources.pin_8, Level::Low);
+    let pin_spi_sclk = split_peripherals.display_resources.pin_10;
+    let pin_spi_mosi = split_peripherals.display_resources.pin_11;
+    let pin_busy = Input::new(
+        split_peripherals.display_resources.pin_13,
+        embassy_rp::gpio::Pull::None,
+    );
+
+    let mut display_config = spi::Config::default();
+    const DISPLAY_FREQ: u32 = 16_000_000;
+    display_config.frequency = DISPLAY_FREQ;
+    display_config.phase = spi::Phase::CaptureOnFirstTransition;
+    display_config.polarity = spi::Polarity::IdleLow;
+
+    let spi_bus = Spi::new_blocking_txonly(
+        split_peripherals.display_resources.spi1,
+        pin_spi_sclk,
+        pin_spi_mosi,
+        display_config,
+    );
+    let mut spi_device: ExclusiveDevice<
+        Spi<'_, embassy_rp::peripherals::SPI1, spi::Blocking>,
+        Output<'_>,
+        Delay,
+    > = ExclusiveDevice::new(spi_bus, pin_cs, Delay).expect("Display: SPI initalise error");
+
+    // Setup the EPD driver
+    let epd_driver = EPD3in7::new(
+        &mut spi_device,
+        pin_busy,
+        pin_data_cmd,
+        pin_reset,
+        &mut Delay,
+        None,
+    )
+    .expect("Display: eink initalise error"); // Force unwrap, as there is nothing that can be done if this errors out
+
+    // Setup the CYW43 Wifi chip
+    info!("{}: Initialising CYW43 Wifi chip...", function_name!());
+    let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+    let pwr = Output::new(split_peripherals.network_resources.pin_23, Level::Low);
+    let cs = Output::new(split_peripherals.network_resources.pin_25, Level::High);
+    let mut pio = Pio::new(split_peripherals.network_resources.pio0, Irqs);
     let spi = PioSpi::new(
         &mut pio.common,
         pio.sm0,
-        RM2_CLOCK_DIVIDER,
+        CHIP_SPECIFIC_CLOCK_DIVIDER,
         pio.irq0,
         cs,
-        p.PIN_24,
-        p.PIN_29,
-        embassy_rp::dma::Channel::new(p.DMA_CH0, Irqs),
-        // embassy_rp::dma::Channel::new(p.DMA_CH1, Irqs),
+        split_peripherals.network_resources.pin_24,
+        split_peripherals.network_resources.pin_29,
+        embassy_rp::dma::Channel::new(split_peripherals.network_resources.dma_ch0, Irqs),
     );
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
-    spawner.spawn(unwrap!(cyw43_task(runner)));
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
+    unwrap!(spawner.spawn(cyw43_task(runner)));
+    debug!("{}: Here!", function_name!());
 
     control.init(clm).await;
     control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .set_power_management(cyw43::PowerManagementMode::Performance)
         .await;
 
+    let config = Config::dhcpv4(Default::default());
+
+    // Generate random seed
+    let mut rng: RoscRng = RoscRng;
+    let seed = rng.next_u64();
+
+    // Init network stack
+    static RESOURCES: StaticCell<StackResources<5>> = StaticCell::new();
+    let (stack, runner) = embassy_net::new(
+        net_device,
+        config,
+        RESOURCES.init(StackResources::new()),
+        seed,
+    );
+
+    unwrap!(spawner.spawn(net_task(runner)));
     let wifi_config = WifiConfig::new();
-    info!("ssid {}", wifi_config.ssid);
-    info!("password {}", wifi_config.password);
-
-    let delay = Duration::from_secs(1);
     loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
+        match control
+            .join(
+                wifi_config.ssid,
+                JoinOptions::new(wifi_config.password.as_bytes()),
+            )
+            .await
+        {
+            Ok(_) => break,
+            Err(err) => {
+                info!("{}: join failed with status={}", function_name!(), err);
+            }
+        }
+    }
 
-        info!("led off!");
+    // Wait for DHCP, not necessary when using static IP
+    info!("{}: waiting for DHCP...", function_name!());
+    while !stack.is_config_up() {
+        Timer::after_millis(100).await;
+    }
+    info!("{}: DHCP is now up!", function_name!());
+
+    info!("{}: waiting for link up...", function_name!());
+    while !stack.is_link_up() {
+        Timer::after_millis(500).await;
+    }
+    info!("{}: Link is up!", function_name!());
+
+    info!("{}: waiting for stack to be up...", function_name!());
+    stack.wait_config_up().await;
+    info!("{}: Stack is up!", function_name!());
+
+    // let loop_delay = Duration::from_secs(59);
+    let blink_delay = Duration::from_millis(500);
+    loop {
+        // Keep the main task alive
+        Timer::after(blink_delay).await;
+        info!("{}: Main task is running...", function_name!());
+
+        // Blink the onboard LED to show that the main task is alive
+        control.gpio_set(0, true).await;
+        Timer::after(blink_delay).await;
         control.gpio_set(0, false).await;
-        Timer::after(delay).await;
     }
 }
