8d10085004d8ec2cebaa520dc46aa43489a963bb
its3mile <81533043+its3mile@users.noreply.github.com>
2026-08-04 21:22:34 +0100

    wip: test code for NTP client
    Untested on device
    Should return unix timestamp
    Need to implement localization to get BST/GMT

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@
 
 use ::function_name::named;
 use assign_resources::assign_resources;
+use core::net::IpAddr;
+use core::net::SocketAddr;
 use cyw43::JoinOptions;
 use cyw43::aligned_bytes;
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use defmt::*;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_net::dns::DnsQueryType;
+use embassy_net::udp::{PacketMetadata, UdpSocket as EmbassyUdpSocket};
 use embassy_net::{Config, StackResources};
 use embassy_rp::bind_interrupts;
 use embassy_rp::clocks::RoscRng;
@@ -28,6 +32,9 @@ use embedded_hal_bus::spi::ExclusiveDevice;
 use epd_waveshare::epd3in7::EPD3in7;
 use epd_waveshare::prelude::WaveshareDisplay;
 use heapless::{String, Vec};
+use sntpc::{NtpContext, get_time};
+use sntpc_net_embassy::UdpSocketWrapper;
+use sntpc_time_embassy::EmbassyTimestampGenerator;
 use static_cell::StaticCell;
 
 mod config;
@@ -265,7 +272,64 @@ async fn main(spawner: Spawner) {
     spawner.spawn(unwrap!(request_task(stack.clone())));
 
     let blink_delay = Duration::from_millis(500);
+    let ntp_delay = Duration::from_secs(60);
+
+    let mut rx_meta = [PacketMetadata::EMPTY; 1];
+    let mut rx_buffer = [0u8; 256];
+    let mut tx_meta = [PacketMetadata::EMPTY; 1];
+    let mut tx_buffer = [0u8; 256];
+    let mut ntp_socket = EmbassyUdpSocket::new(
+        stack.clone(),
+        &mut rx_meta,
+        &mut rx_buffer,
+        &mut tx_meta,
+        &mut tx_buffer,
+    );
+    ntp_socket.bind(0).expect("NTP socket bind failed");
+    let ntp_socket = UdpSocketWrapper::new(ntp_socket);
+
     loop {
+        let ntp_context = NtpContext::new(EmbassyTimestampGenerator::default());
+        let server_addr = match stack.dns_query("pool.ntp.org", DnsQueryType::A).await {
+            Ok(mut results) => {
+                if let Some(ip) = results.pop() {
+                    match ip {
+                        embassy_net::IpAddress::Ipv4(addr) => {
+                            SocketAddr::new(IpAddr::V4(addr), 123u16)
+                        }
+                    }
+                } else {
+                    info!("{}: no DNS response for pool.ntp.org", function_name!());
+                    Timer::after(ntp_delay).await;
+                    continue;
+                }
+            }
+            Err(err) => {
+                info!(
+                    "{}: DNS lookup failed for pool.ntp.org: {:?}",
+                    function_name!(),
+                    err
+                );
+                Timer::after(ntp_delay).await;
+                continue;
+            }
+        };
+
+        match get_time(server_addr, &ntp_socket, ntp_context).await {
+            Ok(time) => {
+                let subseconds = u64::from(time.sec_fraction()) * 1_000_000 / u64::from(u32::MAX);
+                info!(
+                    "{}: NTP time received {}.{}",
+                    function_name!(),
+                    time.sec(),
+                    subseconds
+                );
+            }
+            Err(_) => {
+                info!("{}: NTP request failed", function_name!());
+            }
+        }
+
         // Keep the main task alive
         Timer::after(blink_delay).await;
         info!("{}: Main task is running...", function_name!());
@@ -274,5 +338,7 @@ async fn main(spawner: Spawner) {
         control.gpio_set(0, true).await;
         Timer::after(blink_delay).await;
         control.gpio_set(0, false).await;
+
+        Timer::after(ntp_delay).await;
     }
 }
